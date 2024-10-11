e0af2035cf4b0655062d994d2771a722c7c57ec4
KenAlwyn <ksvalenciano1@up.edu.ph>
2024-10-11 23:21:25 +0800

    feat: add service post model and widget

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,12 +1,41 @@
 import 'package:flutter/material.dart';
+import 'package:million_dollar_startup/widgets/service_post_card.dart';
+import '../dummy/service_posts_data.dart';
 
 class HomePage extends StatelessWidget {
   const HomePage({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Center(
-      child: Text("Home"),
+
+    List<ServicePostCard> servicePostCards = servicePostsDummyData.map((servicePost) {
+      return ServicePostCard(servicePostData: servicePost);
+    }).toList();
+
+    return Scaffold(
+      // AppBar with amber, grey[900], and white theme, and Drawer widget
+      appBar: AppBar(
+        title: const Text(
+          "Home",
+          style: TextStyle(
+            fontFamily: 'Montserrat', // Custom font if needed
+            fontWeight: FontWeight.w600,
+            fontSize: 20,
+            color: Colors.white, // White title text
+          ),
+        ),
+        centerTitle: true, // Centered title
+        backgroundColor: Colors.grey[900], // Grey[900] background color
+        elevation: 5, // Slight shadow effect
+      ),
+      
+      body: ListView.builder(
+        padding: const EdgeInsets.all(8),
+        itemCount: servicePostCards.length,
+        itemBuilder: (context, index) {
+          return servicePostCards[index];
+        },
+      ),
     );
   }
 }
